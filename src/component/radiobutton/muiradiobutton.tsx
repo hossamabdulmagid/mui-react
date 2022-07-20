8bec85b0f79a411e95e87e25295205a320d9857b
= <hosamabdulmaged@gmail.com>
2022-07-20 18:40:08 +0200

    radiobutton

--- src/component/radiobutton/muiradiobutton.tsx
+++ src/component/radiobutton/muiradiobutton.tsx
@@ -1,18 +1,26 @@
-import { Stack, Box, Button,Radio, RadioGroup, FormControl, FormLabel ,FormControlLabel} from '@mui/material';
+import React, { useState } from 'react';
+import { Stack, Box, Button, Radio, RadioGroup, FormControl, FormLabel, FormControlLabel } from '@mui/material';
 const RadioButton = () => {
+
+    const [value, setValue] = useState('');
+    console.log(value, `value`);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+    }
+
     return (
         <Box>
             <FormControl>
                 <FormLabel id='job-expernice-group-label'>
                     Years Of Experince
-                    </FormLabel >
+                </FormLabel >
 
-                    <RadioGroup id='job-expernice-group' aria-labelledby="job-expernice-group-label">
-                        <FormControlLabel  control={<Radio/>}label= '0-2' value='0-2'/>
-                        <FormControlLabel  control={<Radio/>}label= '3-5' value='3-5'/>
-                        <FormControlLabel  control={<Radio/>}label= '6-10' value='6-10'/>
-
-                    </RadioGroup>
+                <RadioGroup id='job-expernice-group' aria-labelledby="job-expernice-group-label" onChange={handleChange}>
+                    <FormControlLabel control={<Radio />} label='0-2' value='0-2' />
+                    <FormControlLabel control={<Radio />} label='3-5' value='3-5' />
+                    <FormControlLabel control={<Radio />} label='6-10' value='6-10' />
+                </RadioGroup>
             </FormControl>
         </Box>
     );

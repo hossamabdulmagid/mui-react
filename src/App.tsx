e9ac50dec4c68580cb5804ff3040861b78fd6afd
= <hosamabdulmaged@gmail.com>
2022-07-20 15:32:55 +0200

    radiobutton,drawer,navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,14 @@ import MuiButton from './component/button/MuiButton';
 import StateTextFields from './component/form/input';
 import RegisterPage from './component/form/formValidtion';
 import MuiSelect from './component/select/select';
-
+import MenuAppBar from './component/navbar/navbar';
+import RadioButton from './component/radiobutton/muiradiobutton'
 const App = () => {
   return (
     <div className="App">
+      <MenuAppBar/>
+      <RadioButton/>
+
       <MuiTypoGraphy  />
       <MuiButton />
       <StateTextFields/>
--- src/component/radiobutton/muiradiobutton.tsx
+++ src/component/radiobutton/muiradiobutton.tsx
@@ -1,9 +1,22 @@
-import{Stack,Button  }from '@mui/material';
-const RadioButton = ()=>{
-    return(
-        <Stack spacing={4} >
+import { Stack, Box, Button,Radio, RadioGroup, FormControl, FormLabel ,FormControlLabel} from '@mui/material';
+const RadioButton = () => {
+    return (
+        <Box>
+            <FormControl>
+                <FormLabel id='job-expernice-group-label'>
+                    Years Of Experince
+                    </FormLabel >
 
-        </Stack>    
-        );
+                    <RadioGroup id='job-expernice-group' aria-labelledby="job-expernice-group-label">
+                        <FormControlLabel  control={<Radio/>}label= '0-2' value='0-2'/>
+                        <FormControlLabel  control={<Radio/>}label= '3-5' value='3-5'/>
+                        <FormControlLabel  control={<Radio/>}label= '6-10' value='6-10'/>
+
+                    </RadioGroup>
+            </FormControl>
+        </Box>
+    );
 }
+
+
 export default RadioButton;

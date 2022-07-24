80311441883750ed0fef09da0a8f271cb2d08a4c
= <hosamabdulmaged@gmail.com>
2022-07-24 14:36:28 +0200

    setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Navbar from './component/navbar/navbar';
-
 import HomePage from './pages/homepage/homepage.component';
 import Footer from './component/footer/footer.component';
+import { Route, Routes } from "react-router-dom";
+import Login from './pages/login/login.component';
+
+
 const App = () => {
   return (
     <div className="App">
       <Navbar label='Login | Signup' />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/pages/homepage/homepage.component.tsx
+++ src/pages/homepage/homepage.component.tsx
@@ -137,7 +137,6 @@ const HomePage = () => {
                                                 {singleLink.title}
                                             </Link>
                                         </Box>
-
                                     )
                                 })}
                             </Box>
